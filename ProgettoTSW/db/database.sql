b556cb5746be39e035e98d7a0b766e078f0c5318
Ciro <ciro.troiano@email.com>
2022-04-28 16:56:05 +0200

    Updated

--- ProgettoTSW/db/database.sql
+++ ProgettoTSW/db/database.sql
@@ -27,10 +27,10 @@ DROP TABLE IF EXISTS `appartenenza`;
 CREATE TABLE `appartenenza` (
   `prodotto` varchar(25) NOT NULL,
   `sottocategoria` varchar(50) NOT NULL,
-  PRIMARY KEY (`prodotto`),
-  KEY `appartenenza_sottocategoriaProdotto_nome_fk` (`sottocategoria`),
-  CONSTRAINT `appartenenza_prodotto_codiceSeriale_fk` FOREIGN KEY (`prodotto`) REFERENCES `prodotto` (`codiceSeriale`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `appartenenza_sottocategoriaProdotto_nome_fk` FOREIGN KEY (`sottocategoria`) REFERENCES `sottocategoria` (`nome`)
+  PRIMARY KEY (`prodotto`,`sottocategoria`),
+  KEY `appartenenza_sottocategoria__fkasda` (`sottocategoria`),
+  CONSTRAINT `appartenenza_prodotto_codiceSeriale_fk` FOREIGN KEY (`prodotto`) REFERENCES `prodotto` (`codiceSeriale`),
+  CONSTRAINT `appartenenza_sottocategoria__fkasda` FOREIGN KEY (`sottocategoria`) REFERENCES `sottocategoria` (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,6 +40,7 @@ CREATE TABLE `appartenenza` (
 
 LOCK TABLES `appartenenza` WRITE;
 /*!40000 ALTER TABLE `appartenenza` DISABLE KEYS */;
+INSERT INTO `appartenenza` VALUES ('gFEfPBJg','biscotti'),('hcCcRJX5','energy drink'),('zC3RFPrT','energy drink'),('GesKKqnx','patatine'),('UoESj9xK','patatine'),('Pz7nszym','snack dolci'),('PZzimtRa','snack dolci'),('xBDiMhYa','snack dolci'),('GesKKqnx','snack salati'),('T4iGqTCG','snack salati'),('UoESj9xK','snack salati'),('tyih29rt','soft drink');
 /*!40000 ALTER TABLE `appartenenza` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,9 +59,9 @@ CREATE TABLE `dettaglioProdotto` (
   `quantita` int NOT NULL,
   `origine` varchar(50) DEFAULT NULL,
   `scadenza` date NOT NULL,
-  `peso` double DEFAULT NULL,
-  `volume` double DEFAULT NULL,
-  `immagine` varchar(100) NOT NULL,
+  `peso` varchar(25) DEFAULT NULL,
+  `volume` varchar(25) DEFAULT NULL,
+  `immagine` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`tipo`,`prodotto`),
   KEY `fk_new_table_1_idx` (`prodotto`),
   CONSTRAINT `fk_new_table_1` FOREIGN KEY (`prodotto`) REFERENCES `prodotto` (`codiceSeriale`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -73,6 +74,7 @@ CREATE TABLE `dettaglioProdotto` (
 
 LOCK TABLES `dettaglioProdotto` WRITE;
 /*!40000 ALTER TABLE `dettaglioProdotto` DISABLE KEYS */;
+INSERT INTO `dettaglioProdotto` VALUES ('snack','GesKKqnx',6,22,99,'Giappone','2023-04-28','50g',NULL,'https://gdurl.com/AHQy/'),('snack','gFEfPBJg',2.99,22,99,'USA','2023-04-26','154g',NULL,'https://gdurl.com/LmYd/'),('snack','Pz7nszym',2.49,22,99,'USA','2023-04-10','49.9g',NULL,'https://gdurl.com/Q7D7/'),('snack','PZzimtRa',1.99,22,99,'USA','2023-04-22','47g',NULL,'https://gdurl.com/JDt7/'),('snack','tyih29rt',1.59,22,99,'UK','2023-04-21',NULL,'330ml','https://gdurl.com/CcXa/'),('snack','UoESj9xK',4.99,22,99,'USA','2023-04-17','226g',NULL,'https://gdurl.com/kxoy/'),('snack','xBDiMhYa',1.99,22,99,'USA','2023-04-20','40g',NULL,'https://gdurl.com/gYjUK/'),('drink','hcCcRJX5',5.99,22,99,'Giappone','2023-04-19',NULL,'355ml','https://gdurl.com/G16E/'),('drink','T4iGqTCG',6.5,22,99,'Giappone','2023-04-07','115g',NULL,'https://gdurl.com/MsbP/'),('drink','zC3RFPrT',1.5,22,99,'Internazionale','2023-04-30',NULL,'500ml','https://gdurl.com/obje/');
 /*!40000 ALTER TABLE `dettaglioProdotto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +89,7 @@ CREATE TABLE `prodotto` (
   `codiceSeriale` varchar(25) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `marca` varchar(50) NOT NULL,
-  `descrizioneBreve` varchar(250) NOT NULL,
+  `descrizioneBreve` varchar(1000) NOT NULL,
   `edLimitata` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`codiceSeriale`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -99,6 +101,7 @@ CREATE TABLE `prodotto` (
 
 LOCK TABLES `prodotto` WRITE;
 /*!40000 ALTER TABLE `prodotto` DISABLE KEYS */;
+INSERT INTO `prodotto` VALUES ('GesKKqnx','Chip Star Potato Chips - Mild Salt','Chip Star YBC','Le Chip Star Potato Chips della YBC sono uno degli snack più famosi. La confezione è un pratico cilindro con chiusura per portarle ovunque! Queste patatine sono leggermente salate per offrire un gusto delicato. Disponibili anche in altre varietà.',0),('gFEfPBJg','Oreo Mint Flavour','Oreo','Biscotti Oreo alla menta, farciti con tutto il gusto della crema green. Uno snack dolce che potrai tenere con te sul posto di lavoro, a scuola, in ufficio oppure in giro.',0),('hcCcRJX5','Monster Cuba Libre','Monster','Bevanda energetica all\'intenso sapore di Cuba Libre, il famoso cocktail al rum, cola e lime. Una lattina davvero speciale, assolutamente da collezione che arriva direttamente dal Giappone.',1),('Pz7nszym','Snickers Almond','Snickers','Barretta di cioccolato Snickers Almond da 49.9 grammi per una candy bar farcita internamente con squisite ed originali mandorle americane. Ma non solo: questo snack dolce è composto',0),('PZzimtRa','Reese\'s Nut Bar','Reese\'s','Barretta di cioccolato Reese\'s Nut bar, con all\'interno crema di caramello e arachidi intere a comporre uno snack dolce fra i migliori e più buoni di tutto il catalogo Reese\'s.',0),('T4iGqTCG','Chip Star Nori-Shio','Chip Star YBC','Patatine Chip Star al gusto di alga marina Nori-shio e sale.',0),('tyih29rt','Dr Pepper','Dr Pepper','Dr Pepper UK, bevanda fruttata con vaniglia e caramello. Un prodotto che non può assolutamente mancare nella tua dispensa, nella versione britannica, sinonimo di storia ed effervescenza! Dr. Pepper infatti è fra le bibite più conosciute ed apprezzate anche negli Stati Uniti e se hai voglia di provare qualcosa mai assaggiato prima non puoi assolutamente fartela scappare!',0),('UoESj9xK','Cheetos Crunchy','Cheetos ','Cheetos, Crunchy Flavoured, arricchiti da un\'incredibile croccantezza in questa straordinaria variante all\'originale e intenso sapore di formaggio cheddar. Le Cheetos Crunchy rappresentano le patatine preferite dagli americani ormai già dal 1948 per via del magico gusto, della forma un po’ bizzarra e della croccantezza unica.',0),('xBDiMhYa','M&M\'s Fudge Brownie','M&M\'s','Confezione di M&M\'s Fudge Brownie, confetti al cioccolato con cuore morbido al brownie, un famoso dolcetto al cioccolato e nocciole. Questa soluzione M&M\'s da 40 grammi contiene tutta la bontà e la qualità dell\'impasto M&M\'s, accompagnato da un dolce cuore al brownie che farà impazzire tutti gli appassionati.',0),('zC3RFPrT','Monster Mango Loco','Monster','Monster Energy al gusto mango è un mix perfetto tra dolcezza ed energia elettrizzante! Lasciati trasportare dalla movida',0);
 /*!40000 ALTER TABLE `prodotto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,6 +124,7 @@ CREATE TABLE `sottocategoria` (
 
 LOCK TABLES `sottocategoria` WRITE;
 /*!40000 ALTER TABLE `sottocategoria` DISABLE KEYS */;
+INSERT INTO `sottocategoria` VALUES ('biscotti'),('energy drink'),('merendine'),('patatine'),('snack dolci'),('snack salati'),('soft drink');
 /*!40000 ALTER TABLE `sottocategoria` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -133,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-28 12:36:13
+-- Dump completed on 2022-04-28 16:55:12
