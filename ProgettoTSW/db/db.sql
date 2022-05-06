0f3a636e5582cb12b5306cd9f7d6f30001f0486d
cirot <cirot@cirot-pc>
2022-05-06 13:41:45 +0200

    Updated

--- ProgettoTSW/db/db.sql
+++ ProgettoTSW/db/db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `eCommerceDB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `eCommerceDB`;
 -- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
 --
 -- Host: localhost    Database: eCommerceDB
@@ -293,4 +295,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-06 13:14:05
+-- Dump completed on 2022-05-06 13:41:06
